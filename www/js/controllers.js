f65b38dd7b47e735f3100d1306903257834c667d
Thomas Plaindoux <thomas.plaindoux@gmail.com>
2014-08-17 17:25:35 +0200

    first commit

---
+++ www/js/controllers.js
@@ -0,0 +1,54 @@
+angular.module('starter.controllers', [])
+
+.controller('DashCtrl', function($resource, $scope, $ionicLoading) {
+	var User = $resource('http://localhost:8081/character/:username', {});
+
+	$scope.search = {};
+	$scope.search.query = '';
+
+	var loading = {
+		init: function (argument) {
+			$ionicLoading.show({
+				content: 'Loading Data',
+				animation: 'fade-in',
+				showBackdrop: true,
+				maxWidth: 200,
+				showDelay: 500
+			});
+		},
+		complete: function (argument) {
+			$ionicLoading.hide();
+		}
+	};
+
+	var getQuery = function () {
+		return $scope.search.query;
+	}
+	var getInfos = function () {
+		$scope.loading = loading.init();
+		var lookfor = getQuery();
+		console.info('Searching: ' + lookfor);
+
+		User.get({username:lookfor},function(results) {
+			$scope.results = results;
+			$scope.loading = loading.complete();
+		});		
+	}
+	getInfos();
+
+	$scope.getInfos = function () {
+		getInfos();
+	};
+
+})
+
+.controller('FriendsCtrl', function($scope, Friends) {
+	$scope.friends = Friends.all();
+})
+
+.controller('FriendDetailCtrl', function($scope, $stateParams, Friends) {
+	$scope.friend = Friends.get($stateParams.friendId);
+})
+
+.controller('AccountCtrl', function($scope) {
+});
